06370ea410704fda0a20ba5d3542993dd9414a18
Edgar Florez <eflorez@hugeinc.com>
2026-02-17 08:59:24 -0500

    Hands on Lab: Book Review Application
        - commands for login, register and getbookreview.
        - Update register and login endpoint
        - Include utility methods for check existing users and check if the login is granted.
        - jwt on login.

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,30 +1,74 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+const doesExist = (username) => {
+  let userWithSameName = users.filter((user) => user.name === username);
+  if (userWithSameName.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const isValid = (username) => {
+  //returns boolean
+  //write code to check is the username is valid
+};
+
+const authenticatedUser = (username, password) => {
+  let validUser = users.filter((user) => {
+    return user.name === username && user.password === password;
+  });
+  if (validUser.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(404).json({ message: "Error logging in" });
+  }
+
+  if (authenticatedUser(username, password)) {
+    // generate the JWT access token
+    let accessToken = jwt.sign(
+      {
+        data: password,
+      },
+      "access",
+      { expiresIn: 60 * 60 },
+    );
+    req.session.autorization = {
+      accessToken,
+      username,
+    };
+    return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({ message: "Invalid credentials" });
+  }
+
+  // console.log("login");
+
+  // //Write your code here
+  // return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
+module.exports.doesExist = doesExist;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,12 +1,25 @@
 const express = require("express");
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
+let doesExist = require("./auth_users.js").doesExist;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 public_users.post("/register", (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const { username, password } = req.body;
+
+  if (username && password) {
+    if (!doesExist(username)) {
+      users.push({ name: username, password: password });
+      return res
+        .status(200)
+        .json({ message: "User successfuly registered. Now you can login" });
+    } else {
+      return res.status(400).json({ message: "User already exists!" });
+    }
+  }
+
+  return res.status(404).json({ message: "Unable to register user." });
 });
 
 // Get the book list available in the shop
